4c6cc6c435e315828e636502c3f3af4a74bfd280
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-09-12 05:24:14 +0800

    Update weixin110.js

--- script/weixin110.js
+++ script/weixin110.js
@@ -7,9 +7,9 @@ let opener = str.indexOf("m.tb.cn") != -1 ? "taobao://" + str: ($response.body)
 const $ = new cmp()
 
 if (str.indexOf("m.tb.cn") != -1) {
-    $.notify(``,"微信助手·","如需访问该链接，请点击通知跳转", opener)
+    $.notify(``,"微信助手·","如需访问该链接，请点击本通知跳转", opener)
 } else if (str.indexOf("如需浏览")) {
-    $.notify(``,"微信助手·","如需访问该链接，请点击通知跳转", "https://"+str)
+    $.notify(``,"微信助手·","如需访问该链接，请点击本通知跳转", "https://"+str)
 }
 
 $done({body: $response.body});
