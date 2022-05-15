88dbcf7821f224e1b9a52964d3f9871fd553baba
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2022-05-15 14:44:06 +0800

    Update coo.js

--- script/coo.js
+++ script/coo.js
@@ -1,10 +1,3 @@
 let obj = JSON.parse(magicJS.response.body);
-   if(obj.data.length>3){
-   delete obj.data[0];
-   delete obj.data[3];
-         }
-   body = JSON.stringify(obj);
-         } catch (err) {
-   magicJS.logError(`屏蔽热搜出现异常：${err}`);
-         }
-   break;
+   delete obj.data;
+$done({body: JSON.stringify(obj)});
