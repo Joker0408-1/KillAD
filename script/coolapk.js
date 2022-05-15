0f04e2148ab467d0d948766dd22b7d38ba07a2e7
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2022-05-15 16:32:25 +0800

    Update coolapk.js

--- script/coolapk.js
+++ script/coolapk.js
@@ -1,6 +1,4 @@
-/*酷安去好物推荐*/
-
-let obj = JSON.parse($response.body);
-
-delete obj.data.include_goods
+var jsonData = {1:关注,2:头条,3:话题,4:快讯,5:酷图,6:新机,7:教程,8:酷品,9:汽车,10:美化,11:热榜,12:问答};
+delete jsonData.8;
+console.log(jsonData);
 $done({body: JSON.stringify(obj)});
