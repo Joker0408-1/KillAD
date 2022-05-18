b779138d1b621b600f7754d7910df6db90ca58f8
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2022-05-18 23:57:05 +0800

    Update coolapk.js

--- script/coolapk.js
+++ script/coolapk.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($response.body);
 delete obj.data.extra_pic;
-delete obj.data.location;
+delete obj.data.related_dyh_ids;
 delete obj.data.extra_title;
 delete obj.data.extraDataArr;
 delete obj.data.include_goods;
