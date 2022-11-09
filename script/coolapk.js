3ccf4ff3211cbed4469a98632a5e94f58ee071ad
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2022-11-09 08:11:40 +0800

    Update and rename coolapk_lite.js to coolapk.js

---
+++ script/coolapk.js
@@ -0,0 +1,8 @@
+let obj = JSON.parse($response.body);
+if (obj.hasOwnProperty("data")) {
+delete obj.data.include_goods
+}
+  $done({ body: JSON.stringify(bodyObj) });
+}else {
+  $done($response);
+}
